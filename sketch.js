d6803bca6c96c56406ff793073215aead83ebc04
Jolyon-Conway <127051917+Jolyon-Conway@users.noreply.github.com>
2023-03-06 22:19:52 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,64 @@
 /** @format */
-
+function preload() {
+	
+}
 let playerPos;
 let bulletsX = [];
 let bulletsY = [];
-let bulletSpeed = 10;
+let bulletSpeed = 5;
 let movementSpeed = 5;
 let bulletCanSpawn = true;
 let score;
 let health;
-
+let shieldHealth = [
+	[[4, 4, 4],[4, 4],[4, 4],[4, 4, 4]],
+	[[4, 4, 4],[4, 4],[4, 4],[4, 4, 4]], 
+	[[4, 4, 4],[4, 4],[4, 4],[4, 4, 4]], 
+	[[4, 4, 4],[4, 4],[4, 4],[4, 4, 4]]
+];
+let shieldPosX = [
+	[[100, 100, 100],[120, 120,],[140, 140],[160,160,160]],
+	[[400, 400, 400],[420, 420],[440, 440],[460, 460, 460]],
+	[[700, 700, 700],[720, 720],[740, 740],[760,760, 760]],
+	[[1000, 1000, 1000],[1020, 1020],[1040, 1040],[1060, 1060, 1060]]
+];
+let shieldPosY = [
+	[[680, 660, 640],[660, 640,],[660, 640],[680, 660, 640]],
+	[[680, 660, 640],[660, 640,],[660, 640],[680, 660, 640]],
+	[[680, 660, 640],[660, 640,],[660, 640],[680, 660, 640]],
+	[[680, 660, 640],[660, 640,],[660, 640],[680, 660, 640]]
+];
+ /* (0, 255, 0); */
+let shieldPictureR = [
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]]
+];
+let shieldPictureG = [
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]]
+]
+let shieldPictureB = [
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]],
+	[[0, 0, 0],[0, 0],[0, 0],[0, 0, 0]]
+]
+let shieldPictureO = [
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]],
+	[[255, 255, 255],[255, 255],[255, 255],[255, 255, 255]]
+]
 
 function setup() {
 	createCanvas(1200, 741);
 	frameRate(60);
 	playerPos = 600;
-    score = 0000
+    score = 0
     health = 3
 }
 
@@ -70,7 +114,55 @@ function draw() {
 			bulletCanSpawn = true;
 		}
 
-		/* collision detection with objects and player bullets */
+			
+	}
+
+
+	/* shields */
+	for (let block = 0; block < shieldPosX.length; block++) {
+		for (let collumns = 0; collumns < shieldPosX[block].length; collumns++) {
+			for (let piece = 0; piece < shieldPosX[block][collumns].length; piece++) {
+				for (let bulletsIndex = 0; bulletsIndex < bulletsX.length; bulletsIndex++){
+					if (bulletsX[bulletsIndex] >= shieldPosX[block][collumns][piece] && bulletsX[bulletsIndex] <= shieldPosX[block][collumns][piece] + 20 && shieldHealth[block][collumns][piece] > 0) {
+						if (bulletsY[bulletsIndex] <= shieldPosY[block][collumns][piece] && bulletsY[bulletsIndex] >= shieldPosY[block][collumns][piece] - 20) {
+							shieldHealth[block][collumns][piece]--;
+							bulletsY.splice(bulletsIndex, 1);
+							bulletsX.splice(bulletsIndex, 1);
+							bulletCanSpawn = true;
+						}
+					}
+				}
+				if (shieldHealth[block][collumns][piece] == 4) {
+					shieldPictureR[block][collumns][piece] = 0;
+					shieldPictureG[block][collumns][piece] = 255;
+					shieldPictureB[block][collumns][piece] = 0;
+				}else if (shieldHealth[block][collumns][piece] == 3) {
+					shieldPictureR[block][collumns][piece] = 255;
+					shieldPictureG[block][collumns][piece] = 240;
+					shieldPictureB[block][collumns][piece] = 3
+				}else if (shieldHealth[block][collumns][piece] == 2) {
+					shieldPictureR[block][collumns][piece] = 255;
+					shieldPictureG[block][collumns][piece] = 121;
+					shieldPictureB[block][collumns][piece] = 3;
+				}else if (shieldHealth[block][collumns][piece] == 1) {
+					shieldPictureR[block][collumns][piece] = 255;
+					shieldPictureG[block][collumns][piece] = 0;
+					shieldPictureB[block][collumns][piece] = 0;
+				} else {
+					shieldPictureR[block][collumns][piece] = 0;
+					shieldPictureG[block][collumns][piece] = 0;
+					shieldPictureB[block][collumns][piece] = 0;
+					shieldPictureO[block][collumns][piece] = 0;
+				}
+				if (shieldHealth[block][collumns][piece] > 0) {
+					stroke(0,0,0,0)
+					fill(shieldPictureR[block][collumns][piece], shieldPictureG[block][collumns][piece], shieldPictureB[block][collumns][piece], shieldPictureO[block][collumns][piece])
+					rect(shieldPosX[block][collumns][piece], shieldPosY[block][collumns][piece], 20, -20)
+				}
+				
+				
+			}
+		}
 	}
 }
 
